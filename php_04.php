9eae9f6a867c94cd7ae24c835afeb687e5a94776
yingsaowalak <ying.saowalakfuangfu@gmail.com>
2024-12-25 22:29:32 +0700

    php

--- php_04.php
+++ php_04.php
@@ -4,9 +4,19 @@
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
-        body{
-            font-family: kanit;
+         body {
+            background-color: #FCFFC1;
+            font-family: 'Kanit';
+        }
+        h1 {
+            color:#F72C5B; 
+        }
+        .h2 {
+            color:#3C552D 
         }
     </style>
         <body>
@@ -16,11 +26,11 @@
         <form action="php_04.php" method="post">
             <div class="mb-3">
                 <label for="start" class="form-label">Start</label>
-                <input type="input" class="form-control" id="start" name="start" placeholder="กรุณากรอก 'ตัวเลข' ที่ต้องการเป็นตัวเริ่มต้น" required>
+                <input type="input" class="form-control" id="start" name="start" placeholder="กรุณากรอก 'ตัวเลข' ที่ต้องการเป็นตัวแรก" required>
             </div>
             <div class="mb-3">
                 <label for="end" class="form-label">End</label>
-                <input type="input" class="form-control" id="end" name="end" placeholder="กรุณากรอก 'ตัวเลข' ที่ต้องการเป็นตัวสิ้นสุด" required>
+                <input type="input" class="form-control" id="end" name="end" placeholder="กรุณากรอก 'ตัวเลข' ที่ต้องการเป็นตัวสุดท้าย" required>
             </div>
             <div class="mb-4">
                 <button class="btn btn-success" type="submit">แสดงผลลัพธ์</button>
