e09c8eb697b0d9a9d38d213b5383179fea0265a9
yingsaowalak <ying.saowalakfuangfu@gmail.com>
2024-12-25 22:27:04 +0700

    php

--- php_02.php
+++ php_02.php
@@ -5,19 +5,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
         body {
-            background-color:rgb(255, 249, 199);
+            background-color: #FCFFC1;
             font-family: 'Kanit';
         }
         h1 {
-            color:rgb(201, 91, 1); 
+            color:#F72C5B; 
         }
-    </style>  
-
-    
+        .h2 {
+            color:#3C552D 
+        }
+    </style>
 </head>
     <body>
     <title>PHP_02</title>
@@ -27,7 +28,7 @@
             for($i=1; $i <= 100; $i++){
                 ?>
                 <div class = "row">
-                <div class="h2 col text-end">เลข <?php echo $i; ?></div>
+                <div class="h2 col text-end">เลข  <?php echo $i; ?> เป็น</div>
                 <div class="h2 col text-start"><?php if($i%2==0){echo "จำนวนคู่";}else{echo "จำนวนคี่";} ?></div>
                 </div>
                 <?php
