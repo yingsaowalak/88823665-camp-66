c82d74769cc1ffb65e9a0bcd17025d65ac7bbfa5
yingsaowalak <ying.saowalakfuangfu@gmail.com>
2025-01-14 00:06:39 +0700

    Laravel - form

--- mylaravel/resources/views/myview.blade.php
+++ mylaravel/resources/views/myview.blade.php
@@ -1,11 +1,14 @@
-<h1>My view</h1>
+<h1>My View</h1>
 <?php echo $value_id; ?>
-{{ $value_id; }} {{$myinput}}
-<from action="{{url('/mycontroller')}}" method="post">
+{{ $value_id; }} {{ $myinput;}}
+<form action="{{url('/mycontroller')}}" 
+    method="post">
     @csrf
     <input type="text" name="myinput">
-    <button type="submit">submit</button>
+    <button type="submit">
+        submit
+    </button>
 </form>
-<?php for($i=0;$i<10;$i++){?>
+<?php for($i=0; $i<10; $i++){?>
     <h1>{{$i}}</h1>
 <?php }?>
