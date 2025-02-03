9ccd2b934b24841cf862b149baffac4643b6588b
yingsaowalak <ying.saowalakfuangfu@gmail.com>
2025-02-03 23:45:29 +0700

    Update register.blade.php

--- mylaravel/resources/views/register.blade.php
+++ mylaravel/resources/views/register.blade.php
@@ -1,107 +1,56 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="login-page">
-<div class="register-box">
-    <div class="register-logo">
-      <a href="../index2.html"><b>Admin</b>LTE</a>
-    </div>
-    <!-- /.register-logo -->
-    <div class="card">
-      <div class="card-body register-card-body">
-        <p class="register-box-msg">Register a new membership</p>
-        <form action="{{url('/register')}}" method="post">
-            @csrf
-          <div class="input-group mb-3">
-            <input type="text" name="name" class="form-control" placeholder="Full Name" />
-            <div class="input-group-text"><span class="bi bi-person"></span></div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control" placeholder="Email" />
-            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password" />
-            <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
-          </div>
-          <!--begin::Row-->
-          <div class="row">
-            <div class="col-8">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                <label class="form-check-label" for="flexCheckDefault">
-                  I agree to the <a href="#">terms</a>
-                </label>
-              </div>
+    <div class="register-page">
+        <div class="register-box">
+            <div class="register-logo">
+                <a href="../index2.html"><b>Admin</b>LTE</a>
             </div>
-            <!-- /.col -->
-            <div class="col-4">
-              <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-primary">Sign In</button>
-              </div>
+            <!-- /.register-logo -->
+            <div class="card">
+                <div class="card-body register-card-body">
+                    <p class="register-box-msg">Register a new membership</p>
+                    <form action="{{ url('/register') }}" method="post">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <input type="text" name="name" class="form-control" placeholder="Full Name" />
+                            <div class="input-group-text"><span class="bi bi-person"></span></div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="email" name="email" class="form-control" placeholder="Email" />
+                            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" name="password" class="form-control" placeholder="Password" />
+                            <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        </div>
+                        <!--begin::Row-->
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        I agree to the <a href="#">terms</a>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary">Sign In</button>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!--end::Row-->
+                    </form>
+                    <!-- /.social-auth-links -->
+                    <p class="mb-0">
+                        <a href="{{ url('/login') }}" class="text-center"> I already have a membership </a>
+                    </p>
+                </div>
+                <!-- /.register-card-body -->
             </div>
-            <!-- /.col -->
-          </div>
-          <!--end::Row-->
-        </form>
-        <div class="social-auth-links text-center mb-3 d-grid gap-2">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-primary">
-            <i class="bi bi-facebook me-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-danger">
-            <i class="bi bi-google me-2"></i> Sign in using Google+
-          </a>
         </div>
-        <!-- /.social-auth-links -->
-        <p class="mb-0">
-          <a href="login.html" class="text-center"> I already have a membership </a>
-        </p>
-      </div>
-      <!-- /.register-card-body -->
     </div>
-  </div>
-  <!-- /.register-box -->
-  <!--begin::Third Party Plugin(OverlayScrollbars)-->
-  <script
-    src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
-    integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
-    crossorigin="anonymous"
-  ></script>
-  <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-  <script
-    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"
-  ></script>
-  <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-    crossorigin="anonymous"
-  ></script>
-  <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-  <script src="../../../dist/js/adminlte.js"></script>
-  <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
-  <script>
-    const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
-    const Default = {
-      scrollbarTheme: 'os-theme-light',
-      scrollbarAutoHide: 'leave',
-      scrollbarClickScroll: true,
-    };
-    document.addEventListener('DOMContentLoaded', function () {
-      const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-      if (sidebarWrapper && typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== 'undefined') {
-        OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
-          scrollbars: {
-            theme: Default.scrollbarTheme,
-            autoHide: Default.scrollbarAutoHide,
-            clickScroll: Default.scrollbarClickScroll,
-          },
-        });
-      }
-    });
-  </script>
-
 @endsection
